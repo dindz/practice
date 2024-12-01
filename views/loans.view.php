533111a3cc09d3099b82a20302a8dd432e1281aa
Dindz <dindoquitor@gmail.com>
2024-12-01 20:35:15 +0800

    initial commit.

---
+++ views/loans.view.php
@@ -0,0 +1,25 @@
+<?php 
+
+require '../partials/head.php';
+require '../partials/nav.php';
+
+
+?>
+
+        <section>
+            <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+                <div class="space-y-4 md:space-y-8">
+                    <h1>Loan Page</h1>
+                </div>
+            </div>
+        </section>
+
+         <section>
+            <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+                <div class="space-y-4 md:space-y-8">
+                    <h1>Section 2</h1>
+                </div>
+            </div>
+        </section>
+
+<?php require '../partials/footer.php'; ?>
